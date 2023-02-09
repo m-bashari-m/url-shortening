6c0526d328a9b50feaeaf3e525b1febf8dfa9a56
m-bashari-m <m-bashari@outlook.com>
2023-02-09 23:04:09 +0330

    add: hero section, modify: button

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Button } from "../"
+import "./hero.css"
+import heroImage from "../../images/illustration-working.svg"
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <section className="hero">
+      <div className="hero--content">
+        <h1>More than just shorter links</h1>
+        <p>Build your brand's recognition adn get detailed insights on how your links are performing.</p>
+        <Button variant="primary" fullRound fullWidth={false}>Get Started</Button>
+      </div>
+
+      <img src={heroImage} alt="an image of a person working with computer" />
+    </section>
   )
 }
 
